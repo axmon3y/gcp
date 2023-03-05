9ce7dfa96a9fddb64f29afc6de7a6a81d7e16927
xMon3y <120130326+axmon3y@users.noreply.github.com>
2023-03-06 01:57:15 +0700

    Update gcpwin10pro.sh

--- gcpwin10pro.sh
+++ gcpwin10pro.sh
@@ -3,4 +3,4 @@
 ip=$(ip -o addr show | awk '{print $4}' | cut -d '/' -f 1 | grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)')
 gw=("10.$((${-+"(${ip//./"+256*("}))))"}>>8&255)).0.1")
 
-wget --no-check-certificate -qO InstallNET.sh 'https://imgstore.me/InstallNET.sh' && bash InstallNET.sh --ip-addr $ip --ip-mask 255.255.255.0 --ip-gate $gw -dd 'https://oss.sunpma.com/Windows/Whole/Win10_Pro_20h2_19042.630_x64_administrator_Teddysun.com.gz'
+wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/axmon3y/gcp/main/InstallNET.sh' && bash InstallNET.sh --ip-addr $ip --ip-mask 255.255.255.0 --ip-gate $gw -dd 'https://oss.sunpma.com/Windows/Whole/Win10_Pro_20h2_19042.630_x64_administrator_Teddysun.com.gz'
